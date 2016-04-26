266aba4e810a7c3e5dbe8648240ef3b5e5e0d3e7
dreamapple <2451731631@qq.com>
2016-04-26 14:52:42 +0800

    add interpolations

--- guide/4.data-binding-syntax/4.1.interpolations/4.1.0.js
+++ guide/4.data-binding-syntax/4.1.interpolations/4.1.0.js
@@ -1,7 +1,16 @@
+var raw_html = '<div>Hello, World!</div>';
+var id = 1;
+var url = 'http://www.baidu.com';
+var boolValue = true;
+
 var vm = new Vue({
   el: '#app',
   data: {
-    msg: 'Hello,World!'
+    msg: 'Hello,World!',
+    rawHtml: raw_html,
+    id: id,
+    url: url,
+    boolValue: boolValue
   },
   methods: {
     changeMsg: function() {
