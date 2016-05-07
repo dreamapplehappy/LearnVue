e5c0450826e5f2dfaf1e758bd5e1cb18e2f206ef
dreamapple <2451731631@qq.com>
2016-05-07 13:14:25 +0800

    init

--- guide/12.components/12.0.js
+++ guide/12.components/12.0.js
@@ -1,8 +1,36 @@
+// 全局的一个组件
 var myComponentOne = Vue.extend({
-  template: '<div>The first component</div>'
+  props: [
+    'myMsg'
+  ],
+  template: '<div>The first component msg: {{myMsg}}</div>'
+});
+// 子组件
+var child = Vue.extend({
+  props: ['childMsg'],
+  template: '<div>This part is child msg: {{childMsg}}</div>'
+});
+// 父组件
+var parent = Vue.extend({
+  data: function() {
+    return {
+      parentMsg: ':Parent Msg:'
+    }
+  },
+  template: '<div>\
+  <div>This part is parent, parent self data: {{parentMsg}}</div>\
+             <div>\
+             <input type="text" v-model="parentMsg">\
+             <child :child-msg="parentMsg"></child>\
+             </div>\
+  </div>',
+  components: {
+    child: child
+  }
 });
 
 Vue.component('my-component-one', myComponentOne);
+Vue.component('parent', parent);
 
 
 var vm = new Vue({
